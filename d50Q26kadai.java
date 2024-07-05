36cd4983ae86f14a4ad23bbe65d0539721ea9df6
s23022 <s23022@std.it-college.ac.jp>
2024-07-05 03:10:56 +0000

    課題

---
+++ d50Q26kadai.java
@@ -0,0 +1,15 @@
+// s23022
+//d50Q26の課題
+
+enum Condition {
+    ROAD, MUD, SNOW
+}
+
+public class d50Q26kadai {
+    public static void main(String[] args) {
+        for (int i = 0; i < Condition.values().length; i++) {
+            System.out.println(Condition.values()[i]);
+        }
+    }
+}
+
